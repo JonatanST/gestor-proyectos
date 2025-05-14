4ac5583e514119a304a4a2a2c0af55572cc60344
kmape-ASD <kmape@grupoasd.com>
2025-05-14 10:13:10 -0500

    feat: se cambiaron archivos .gitkeep por .ts ADC-493 #time 2m

---
+++ src/app/shared/directives/info.ts
@@ -0,0 +1,21 @@
+/*
+ Licencia: 
+  * Archivo: LICENSE
+  * Fecha: 2025
+  * Todos los derechos de propiedad intelectual e industrial sobre esta
+  * aplicacion son de propiedad exclusiva del GRUPO ASD S.A.S.
+  * Su uso, alteracion, reproduccion o modificacion sin el debido
+  * consentimiento por escrito de GRUPO ASD S.A.S. quedan totalmente prohibidos.
+  *
+  * Este programa se encuentra protegido por las disposiciones de la
+  * Ley 23 de 1982 y demas normas concordantes sobre derechos de autor y
+  * propiedad intelectual. Su uso no autorizado dara lugar a las sanciones
+  * previstas en la Ley.
+  * @FilePath: 
+ */
+/*
+# Aquí se colocarán las Directivas Standalone Reutilizables. 
+# Estas directivas añaden comportamiento o modifican la apariencia de los elementos del DOM y están diseñadas para 
+# ser usadas en múltiples features o componentes no relacionados. 
+# Deben ser importadas en el array imports de los componentes standalone que las utilicen.
+*/
